6029fb8d55c6fb8d069d766827b87a4bf2b03bec
naoangel <es.veneros@gmail.com>
2017-10-05 10:09:51 -0300

    cifrar descifrar

--- app.js
+++ app.js
@@ -17,24 +17,32 @@ function main(){
  
   
 function cipher(){
-    var code = prompt("Ingrese el código a trabajar"); //solicita código a 
+    var code = prompt("Ingrese el código a trabajar"); //solicita código  
 	var codeAscii =[];  // Var para los números del código
     var asciiTo =[]; //Var para tener los números transformados por la fórmula.                  
     var newCode =[];  //Var para tener las letras, resultado.
          for(i=0; i<code.length; i++){ //for para recorrer el código
 		codeAscii[i] = code[i].charCodeAt();  // en codeAscii guardo cada valor de code en número de ASCII                                      
          asciiTo[i] = (codeAscii[i] - 65 + 33)%26 + 65;   //tomar cada valor y transformarlo
-         newCode[i] = asciiTo[i].fromCharCode(); // volverlo a letra otra vez.
+         newCode[i] = String.fromCharCode(asciiTo[i]); // volverlo a letra otra vez.
 
 	}
-	 	   return newCode;
+	 	   return newCode.join('');
 	 	}
  
 
-	
 
-function decipher(code){
+function decipher(){
     var code = prompt("Ingrese el código a trabajar"); //solicita código a 
-	var newCodeNum = "";
+	var code2 = []; //Var para tener los números transformados por la fórmula. 
+	var code3 = []; //Var para tener las letras, resultado.
+    var finalCode = []; // se guarda en esta var el código resultado. 
+	for(i=0; i<code.length; i++){ //for para recorrer el código
+		code2[i] = code[i].charCodeAt();  // en codeAscii guardo cada valor de code en número de ASCII                                      
+        code3[i] = (code2[i] -65 - 7 + 52)%26 + 65;  //aplica fórmula
+        finalCode[i] = String.fromCharCode(code3[i]); // volverlo a letra otra vez.
+
+	}
+	 	   return finalCode.join('');  
 
 }
